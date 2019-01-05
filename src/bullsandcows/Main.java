6c3af2b858b01e26e7419aeb1dd70be715248d6c
VladyaRazumist <45969614+VladyaRazumist@users.noreply.github.com>
2019-01-05 17:55:35 +0700

    Update Main.java

--- src/bullsandcows/Main.java
+++ src/bullsandcows/Main.java
@@ -1,7 +1,36 @@
 package bullsandcows;
 
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+       int cows=0;
+       int bulls=0;
+        Scanner sc = new Scanner (System.in);
+        String s=sc.nextLine();
+        long k=SecretGenerator.Generate(s);
+        long l;
+        String st=Long.toString(k);
+        while (true) {
+        System.out.println("Guess!");
+       if  ((l=sc.nextLong())==k) {
+            break;
+        }
+        String next=Long.toString(l);
+           for (int i=0;i<st.length();i++){
+               for (int j=0;j<next.length();j++){
+                   if (st.charAt(i)==next.charAt(j)){
+                       if(i==j){
+                           bulls++;
+                       } else {
+                           cows++;
+                       }
+                   }
+                   
+               }
+              
+           }
+           System.out.println("Bulls ="+bulls+" and Cows =" + cows);
+        }
+        System.out.println("Right");
     }
 }
